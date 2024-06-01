d64108c5c8256d36f31263c43e638522da7cffaa
Sofa <squishysparrow7.7@gmail.com>
2024-06-01 12:48:16 +1200

    Rule for package declaration
    I also included some needed placeholders

--- tree-sitter-fearless/grammar.js
+++ tree-sitter-fearless/grammar.js
@@ -2,7 +2,18 @@ module.exports = grammar({
   name: 'fearless',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => ''
+    source_file: $ => seq($.package, repeat($.alias), repeat($.topDec)),
+    // TODO: Alias
+    alias: $ => 'alias',
+    // TODO: topDec
+    topDec: $ => 'topDec',
+    // XXX: Do we want something like tree-sitter-java, where each path of the
+    //      package is its own scoped identifier?
+    package: $ => seq('package ', $.packageName, '\n'),
+    packageName: $ => seq(repeat(seq($._px, '.')), $._px),
+    _px: $ => seq($._idLow, repeat($._idChar)),
+    _idLow: $ => choice(/_*[a-z]/, /_+[0-9]/),
+    _idUp: $ => /_*[A-Z]/,
+    _idChar: $ => /[a-zA-Z_0-9]/,
   }
 });
