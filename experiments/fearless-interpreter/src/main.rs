b5ae75eb794768bb21c9609fb0b32e692070aee1
Nick Webster <nick@nick.geek.nz>
2024-09-16 23:17:19 +1200

    Printing all type def names

--- experiments/fearless-interpreter/src/main.rs
+++ experiments/fearless-interpreter/src/main.rs
@@ -12,5 +12,11 @@ fn main() {
     let reader = TypedReader::<_, schema_capnp::package::Owned>::new(reader);
     let pkg_reader = reader.get().unwrap();
     let pkg_name = std::str::from_utf8(pkg_reader.get_name().unwrap().0).unwrap();
+    pkg_reader.get_defs().unwrap().iter().for_each(|def| {
+        let name = def.get_name().unwrap();
+        let def_name = std::str::from_utf8(name.get_name().unwrap().0).unwrap();
+        let arity = name.get_gens();
+        println!("def: {}/{}", def_name, arity);
+    });
     println!("Hello, world! {}", pkg_name);
 }
